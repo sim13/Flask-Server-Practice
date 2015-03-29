17cf0dde5024788fa68620f496cef927b1896541
Simrin Wahal <simrinwahal@Simrins-MacBook-Pro.local>
2015-03-29 02:15:16 -0500

    Added response headers

--- flask-rest.py
+++ flask-rest.py
@@ -2,19 +2,21 @@ from flask import Flask
 from flask_restful import Resource , Api 
 
 app =Flask(__name__)
-api =Api(app)
+api =Api(app, catch_all_404s =True)
 
-User_info {
-	
-	'username' : 'Simrin Wahal' ,
-	'user_id'  : 	'1313' , 
-
-}
 class HelloWorld(Resource):
 	def get(self) :
 	 return {'hello' :'world'}
+class TestResponse(Resource) :
+	def get(self):
+		return {'Etag' :'some-opaque-string'}
+@app.after_request
+def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response 
 
 api.add_resource(HelloWorld, '/json')
+api.add_resource(TestResponse , '/testresponse')
 
 if __name__ == '__main__' :
   app.run(debug=True)
